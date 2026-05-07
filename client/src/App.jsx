eade994aa897293b240f4045a333b8875e3a5fc9
Mongd Amasha <mongdamasha07@gmail.com>
2026-05-07 14:46:08 +0300

    add login screen #7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+import LoginScreen from './components/LoginScreen'
 import TeacherDashboard from './components/TeacherDashboard'
 import './App.css'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  if (!isLoggedIn) {
+    return <LoginScreen onLogin={() => setIsLoggedIn(true)} />
+  }
+
   return <TeacherDashboard />
 }
 
